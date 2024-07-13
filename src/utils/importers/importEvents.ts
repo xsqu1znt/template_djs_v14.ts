3c7bc3944c041ee9fa345a30000176db91a87023
xsqu1zn't <fluffyspiderstudios@gmail.com>
2024-07-13 01:43:20 -1000

    progress

--- src/utils/importers/importEvents.ts
+++ src/utils/importers/importEvents.ts
@@ -9,47 +9,61 @@ async function importEventModules() {
     let files = jt.readDir(path.join(__dirname, "../../events"), { recursive: true });
 
     // Import the files found in the given directory
-    let events: EventModule[] = ((await Promise.all(files.map(fn => import(`../../events/${fn}`)))) as EventModuleExport[])
-        // Destructure the imported event to get the callback
-        .map(e => e.default);
+    // `../../events/${fn}`
+    let events = await Promise.all(
+        files.map(async fn => {
+            let _path: string = path.join(__dirname, "../../events", fn);
+            let _module: EventModule = (await import(_path)).default;
+
+            return { module: _module, path: _path };
+        })
+    );
+
+    // Destructure the imported event to get the callback and path
+    // let events_destructured = events.map(e => ({event: e.default, path: path.join(__dirname, "../../events", fn)}));
 
     // Filter out files that don't have an eventType property or is disabled
-    events.filter(f => f.eventType !== undefined && f.enabled !== false);
     return events;
 }
 
-export default async function (client: Client) {
+export default async function (client: Client): Promise<void> {
     // Import event files
     let events = await importEventModules();
-    if (!events.length) logger.error("[CLIENT] No event modules found", `dir: '${path.join(__dirname, "../../events")}'`);
+    if (!events.length) {
+        logger.error("[CLIENT] No event modules found", `dir: '${path.join(__dirname, "../../events")}'`);
+        return;
+    }
 
-    // Get an array of every EventType
-    let eventTypes = jt.unique(events.map(e => e.eventType));
+    // Get an array of every EventType imported
+    let eventTypes = jt.unique(events.map(e => e.module.eventType));
 
     // Group events by EventType
-    let eventsGrouped = eventTypes.map(type => events.filter(e => e.eventType === type));
+    let eventsGrouped = eventTypes.map(type => events.filter(e => e.module.eventType === type));
 
     // Iterate through grouped events
     for (let group of eventsGrouped) {
         // Iterate through events inside the group
         for (let event of group) {
-            try {
-                // Bind it to the appropriate listener
-                client.on(event.eventType, async (...args) => {
-                    try {
-                        // Execute the event
-                        await event.execute.apply(null, [client, ...args]);
-                    } catch (err) {
-                        // prettier-ignore
-                        // Catch execution errors
-                        logger.error("[CLIENT] Failed to execute function", `'${event.name}' on event '${event.eventType}'`, err);
-                    }
-                });
-            } catch (err) {
-                // prettier-ignore
-                // Invalid event type recieved
-                logger.error("[CLIENT] Failed to bind event", `invalid event: '${event.eventType}' for function '${event.name}'`, err);
+            // Ignore binding events that are disabled
+            if (!event.module.enabled) {
+                logger.client.eventBinded(event.module.name, event.path, false);
+                continue;
             }
+
+            // Bind it to the appropriate listener
+            client.on(event.module.eventType, async (...args) => {
+                try {
+                    // Execute the event
+                    await event.module.execute.apply(null, [client, ...args]);
+                } catch (err) {
+                    // prettier-ignore
+                    // Catch execution errors
+                    logger.error("[CLIENT] Failed to execute function", `'${event.module.name}' on event '${event.module.eventType}'`, err);
+                }
+            });
+
+            // Log to the console that the event was successfully loaded
+            logger.client.eventBinded(event.module.name, event.path, true);
         }
     }
 }
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,42 +6,22 @@ import chalk from "chalk";
 import { name as PROJECT } from "@pkgJSON";
 
 /* - - - - - { Shorthand } - - - - - */
-const _timestamp = () => chalk.bold(`[${new Date().toLocaleTimeString()}]`);
-const _client = () => chalk.bold.gray("[CLIENT]");
+const _client = (): string => chalk.bold.gray("[CLIENT]");
+const _event = (): string => chalk.bold.yellow("[EVENT]");
 
-const _dynamic_shard = (shards: Shard[]) =>
-    `${shards?.length ? chalk.gray(`(${shards.length === 1 ? "Shard:" : "Shard:"} ${shards.join(", ")})`) : ""}`;
-const _shard_count = (count: number) => `${count ? chalk.gray(`Shards running: ${count}`) : ""}`;
+const _timestamp = (): string => chalk.bold(`[${new Date().toLocaleTimeString()}]`);
 
-/* - - - - - { Function Groups } - - - - - */
-export const client = {
-    // prettier-ignore
-    initializing: function (shards?: Shard[]) {
-        console.log(`${_timestamp()} ${_client()} ⏳ ${chalk`{italic Initalizing...} ${_dynamic_shard(shards || [])}`}`);
-    },
+const _dynamic_shard = (shards: Shard[]): string =>
+    shards?.length ? chalk.gray`(${shards.length === 1 ? "Shard:" : "Shards:"} ${shards.join(", ")})` : "";
+const _shard_count = (count: number): string => `${count ? chalk.gray(`Shards running: ${count}`) : ""}`;
 
-    // prettier-ignore
-    conecting: function (shards?: Shard[]) {
-        console.log(`${_timestamp()} ${_client()} ⏳ ${chalk`{italic Connecting to Discord...} ${_dynamic_shard(shards || [])}`}`);
-    },
-
-    // prettier-ignore
-    online: function (shardCount = 0) {
-        console.log(`${_timestamp()} ${_client()} ✅ ${chalk`{italic {green Successfuly connected to Discord!}} ${_shard_count(shardCount)}`}`);
-    },
-
-    // prettier-ignore
-    ready: function (shards?: Shard[]) {
-        console.log(`${_timestamp()} 🎉 ${chalk.blueBright(PROJECT)} is up and running! ${_dynamic_shard(shards || [])}`);
-    }
-};
-
+/* - - - - - { Exports } - - - - - */
 export function debug(msg: string): void {
     console.log(chalk.magenta(msg));
 }
 
 export function error(header: string, msg: string, err: any = ""): void {
-    console.error(chalk.black.bgRed(header) + " " + chalk.magenta(msg), err);
+    console.error(`${chalk.bgRed.black(header)} ${chalk.magenta(msg)}`, err);
 }
 
 export function log(msg: string): void {
@@ -51,3 +31,49 @@ export function log(msg: string): void {
 export function success(msg: string): void {
     console.log(chalk.green(msg));
 }
+
+export const client = {
+    initializing: (shards?: Shard[]): void => {
+        console.log(
+            `$_TIMESTAMP $_CLIENT ⏳ ${chalk.italic("Initalizing...")} $_DYNAMIC_SHARD`
+                .replace("$_TIMESTAMP", _timestamp())
+                .replace("$_CLIENT", _client())
+                .replace("_$DYNAMIC_SHARD", _dynamic_shard(shards || []))
+        );
+    },
+
+    conecting: (shards?: Shard[]): void => {
+        console.log(
+            `$_TIMESTAMP $_CLIENT ⏳ ${chalk.italic("Connecting to Discord...")} $_DYNAMIC_SHARD`
+                .replace("$_TIMESTAMP", _timestamp())
+                .replace("$_CLIENT", _client())
+                .replace("_$DYNAMIC_SHARD", _dynamic_shard(shards || []))
+        );
+    },
+
+    online: (shardCount = 0): void => {
+        console.log(
+            `$_TIMESTAMP $_CLIENT ✅ ${chalk.italic.green("Successfuly connected to Discord!")} $_SHARD_COUNT`
+                .replace("$_TIMESTAMP", _timestamp())
+                .replace("$_CLIENT", _client())
+                .replace("$_SHARD_COUNT", _shard_count(shardCount))
+        );
+    },
+
+    // prettier-ignore
+    ready: (shards?: Shard[]): void => {
+        console.log(
+            `$_TIMESTAMP 🎉 ${chalk.blueBright(PROJECT)} is up and running! $_DYNAMIC_SHARD`
+                .replace("$_TIMESTAMP", _timestamp())
+                .replace("_$DYNAMIC_SHARD", _dynamic_shard(shards || []))
+        );
+    },
+
+    eventBinded: (name: string, path: string, enabled: boolean = false): void => {
+        console.log(
+            `$_TIMESTAMP $_EVENT ${enabled ? "Loaded" : chalk.bgRed("Ignored")}: ${chalk.bold(name)} | Path: ${chalk.bgGray.white(path)}`
+                .replace("$_TIMESTAMP", _timestamp())
+                .replace("$_CLIENT", _client())
+        );
+    }
+};
