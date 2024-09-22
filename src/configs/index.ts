0d030f6b02323a266611e11b37a4b4a1ad3acb7e
squ1dggly <fluffyspiderstudios@gmail.com>
2024-09-21 18:17:33 -1000

    eek

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,3 +1,4 @@
 import config_client from "./config_client.json";
+import config_logger from "./config_logger.json";
 
-export default { client: config_client };
+export default { client: config_client, logger: config_logger };
---
+++ src/utils/loggerRefactored.ts
@@ -0,0 +1,8 @@
+/** @file Reusable functions for using `console.log()`, but in 4k ultra HD retrocolor. */
+
+import { Shard } from "discord.js";
+import jt from "@utils/jsTools";
+import chalk from "chalk";
+
+import { name as PROJECT } from "@pkgJSON";
+import config from "@configs";
