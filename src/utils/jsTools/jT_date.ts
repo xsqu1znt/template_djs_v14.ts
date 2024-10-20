624cdf8965f38e646434b7a440289d7d0f6501cc
squ1dggly <fluffyspiderstudios@gmail.com>
2024-10-19 14:27:07 -1000

    progress

--- src/utils/jsTools/jT_date.ts
+++ src/utils/jsTools/jT_date.ts
@@ -1,5 +1,3 @@
-import __number from "./jT_number";
-
 interface ParseTimeOptions {
     /** Return "s" (seconds) or "ms" (milliseconds). */
     type?: "ms" | "s";
@@ -7,10 +5,22 @@ interface ParseTimeOptions {
     fromNow?: boolean;
 }
 
-/**
- * Parse a string into either milliseconds or seconds
- * @param {string|number} str string to parse
- * @param {ParseTimeOptions} options
+interface ETAOptions {
+    /** The anchor to go off of, a unix timestamp in milliseconds. `Date.now()` is default */
+    since?: number | string;
+    /** Leaves out "ago" if the result is in the past. */
+    ignorePast?: boolean;
+    /** Returns `null` if `unix` is before `since`. */
+    nullIfPast?: boolean;
+    /** The number of decimal places to round the result to. `0` is default. */
+    decimalLimit?: number;
+}
+
+import __number from "./jT_number";
+
+/** Parse a string into either milliseconds or seconds
+ * @param str string to parse
+ * @param options
  *
  * @example
  * parse("1m") --> 60000
@@ -64,24 +74,10 @@ export function parseTime(str: string | number, options?: ParseTimeOptions) {
     return sum;
 }
 
-interface ETAOptions {
-    /** The anchor to go off of, a unix timestamp in milliseconds. `Date.now()` is default */
-    since?: number | string;
-    /** Leaves out "ago" if the result is in the past. */
-    ignorePast?: boolean;
-    /** Returns `null` if `unix` is before `since`. */
-    nullIfPast?: boolean;
-    /** The number of decimal places to round the result to. `0` is default. */
-    decimalLimit?: number;
-}
-
-
-/**
- * Parses the time difference between a given Unix timestamp and a reference point into a human-readable string.
- * 
- * @param unix - The Unix timestamp in milliseconds for which the time difference is calculated.
- * @param options - An optional object to configure the behavior of the function.
- * 
+/** Parses the time difference between a given Unix timestamp and a reference point into a human-readable string.
+ * @param unix The Unix timestamp in milliseconds for which the time difference is calculated.
+ * @param options An optional object to configure the behavior of the function.
+ *
  * @example
  * eta(1703001733955) --> "1 hour" (from now)
  * eta(1702994533936, { nullIfPast: true }) --> null */
@@ -132,4 +128,182 @@ function eta(unix: number | string, options?: ETAOptions): string | null {
     return `${difference} ${result.name}${isPast && !_options.ignorePast ? " ago" : ""}`;
 }
 
-export default { parseTime, eta };
+/** Parses the time difference between a given Unix timestamp and a reference point into a dynamic "H, M, and S" string format.
+ * @param unix The Unix timestamp in milliseconds for which the time difference is calculated.
+ * @param options An optional object to configure the behavior of the function.
+ *
+ * @example
+ * etaHMS(1703001733955) // returns "1 hour, 0 minutes, and 0 seconds" (from now)
+ * etaHMS(1702994533936, { nullIfPast: true }) // returns null */
+function etaHMS(unix: number | string, options?: ETAOptions): string | null {
+    const _options = {
+        ignorePast: false,
+        nullIfPast: false,
+        decimalLimit: 0,
+        ...options,
+        since: options?.since ? Number(options.since) : Date.now()
+    };
+    const _unix = Number(unix);
+
+    /* Check if unix is in the past */
+    const isPast = _unix - _options.since < 0;
+    if (_options.ignorePast && isPast) return null;
+
+    let difference: number | string = Math.abs(_unix - _options.since);
+    /* Return if there's no difference */
+    if (!difference && _options.nullIfPast) return null;
+    if (!difference) return "now";
+
+    /* - - - - - { Preform Calculations } - - - - - */
+    const seconds = __number.msToSec(difference);
+
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = Math.floor((seconds % 3600) % 60);
+
+    const h_f = h > 0 ? `${h} ${h === 1 ? "hour" : "hours"}` : "";
+    const m_f = m > 0 ? `${m} ${m === 1 ? "minute" : "minutes"}` : "";
+    const s_f = s > 0 ? `${s} ${s === 1 ? "second" : "seconds"}` : "";
+
+    const result = [];
+
+    if (h) result.push(h_f);
+    if (m) result.push(m_f);
+    if (s) result.push(s_f);
+
+    // Grammar adjustment
+    if (result.length > 1) result.splice(-1, 0, "and");
+
+    return result.join(", ").replace("and,", "and");
+}
+
+/** Format a unix timestamp into a dynamic "Y, MTH, D, H, M, and S" time string format.
+ * @param unix The Unix timestamp in milliseconds for which the time difference is calculated.
+ * @param options An optional object to configure the behavior of the function.
+ *
+ * @copyright *Code written by **@fujimori_*** */
+function etaYMDHMS(unix: number | string, options?: ETAOptions): string | null {
+    const _options = {
+        ignorePast: false,
+        nullIfPast: false,
+        decimalLimit: 0,
+        ...options,
+        since: options?.since ? Number(options.since) : Date.now()
+    };
+    const _unix = Number(unix);
+
+    /* Check if unix is in the past */
+    const isPast = _unix - _options.since < 0;
+    if (_options.ignorePast && isPast) return null;
+
+    let difference: number | string = Math.abs(_unix - _options.since);
+    /* Return if there's no difference */
+    if (!difference && _options.nullIfPast) return null;
+    if (!difference) return "now";
+
+    /* - - - - - { Preform Calculations } - - - - - */
+    const seconds = __number.msToSec(difference);
+
+    /* Break down the counter math */
+    const y = Math.floor(seconds / 31536000);
+    const mo = Math.floor((seconds % 31536000) / 2628000);
+    const d = Math.floor(((seconds % 31536000) % 2628000) / 86400);
+    const h = Math.floor((seconds % (3600 * 24)) / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = Math.floor(seconds % 60);
+
+    /* Format the time */
+    const yDisplay = y > 0 ? y + (y === 1 ? " year" : " years") : "";
+    const moDisplay = mo > 0 ? mo + (mo === 1 ? " month" : " months") : "";
+    const dDisplay = d > 0 ? d + (d === 1 ? " day" : " days") : "";
+    const hDisplay = h > 0 ? h + (h === 1 ? " hour" : " hours") : "";
+    const mDisplay = m > 0 ? m + (m === 1 ? " minute" : " minutes") : "";
+    const sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
+
+    // Combine the time in an array
+    let result = [];
+
+    if (y) result.push(yDisplay);
+    if (mo) result.push(moDisplay);
+    if (d) result.push(dDisplay);
+    if (h) result.push(hDisplay);
+    if (m) result.push(mDisplay);
+    if (s) result.push(sDisplay);
+
+    // Shorten time
+    if (result.includes(yDisplay)) {
+        result.length = 3;
+    } else if (result.includes(moDisplay)) {
+        result.length = 3;
+    } else if (result.includes(dDisplay)) {
+        result.length = 3;
+    }
+
+    // Filter out empty results
+    result = result.filter(f => f);
+
+    // Grammar adjustment
+    if (result.length > 1) result.splice(-1, 0, "and");
+
+    return result.join(", ").replace("and,", "and");
+}
+
+/**Format a Unix timestamp into a dynamic "DD:HH:MM:SS" time string format.
+ * @param unix The Unix timestamp in milliseconds to convert.
+ * @param options An optional object to configure the behavior of the function.
+ * @copyright *Code written by **@fujimori_*** */
+function etaDigitial(unix: number | string, options?: ETAOptions): string | null {
+    const _options = {
+        ignorePast: false,
+        nullIfPast: false,
+        decimalLimit: 0,
+        ...options,
+        since: options?.since ? Number(options.since) : Date.now()
+    };
+    const _unix = Number(unix);
+
+    /* Check if unix is in the past */
+    const isPast = _unix - _options.since < 0;
+    if (_options.ignorePast && isPast) return null;
+
+    let difference: number | string = Math.abs(_unix - _options.since);
+    /* Return if there's no difference */
+    if (!difference && _options.nullIfPast) return null;
+    if (!difference) return "now";
+
+    /* - - - - - { Preform Calculations } - - - - - */
+    const seconds = __number.msToSec(difference);
+
+    // Break down the counter math
+    let d = Math.floor((seconds % 31536000) / 86400);
+    let h = Math.floor((seconds % (3600 * 24)) / 3600);
+    let m = Math.floor((seconds % 3600) / 60);
+    let s = Math.floor(seconds % 60);
+
+    /* Check if the time is negative */
+    if (d < 0) d = Math.abs(d);
+    if (h < 0) h = Math.abs(h);
+    if (m < 0) m = Math.abs(m);
+    if (s < 0) s = Math.abs(s);
+
+    /* Add zeros in front of the number if needed */
+    const dDisplay = `0${d}`.slice(-2);
+    const hDisplay = `0${h}`.slice(-2);
+    const mDisplay = `0${m}`.slice(-2);
+    const sDisplay = `0${s}`.slice(-2);
+
+    // Combine the time in an array
+    let result = [];
+
+    if (d > 0) result.push(dDisplay);
+    if (h > 0 || d > 0) result.push(hDisplay);
+    if (m) result.push(mDisplay);
+    if (s) result.push(sDisplay);
+
+    // Filter out empty results
+    result = result.filter(f => f);
+
+    return result.join(":");
+}
+
+export default { parseTime, eta, etaHMS, etaYMDHMS, etaDigitial };
