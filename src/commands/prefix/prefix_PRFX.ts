fe058a104e168be3940526021430764e2bce51c3
squ1dggly <fluffyspiderstudios@gmail.com>
2024-10-26 15:55:08 -1000

    added command to view/set prefix

---
+++ src/commands/prefix/prefix_PRFX.ts
@@ -0,0 +1,33 @@
+import { PrefixCommand } from "@customTypes/commands";
+
+import { guildManager } from "@utils/mongo";
+
+export const __command: PrefixCommand = {
+    name: "prefix",
+    description: "Set the prefix for the current server.",
+    category: "Utility",
+    options: { emoji: "⚙️", guildOnly: true, requiredUserPerms: ["Administrator"] },
+
+    execute: async (client, message, { cleanContent }) => {
+        if (!message.inGuild()) return;
+
+        const prefix = cleanContent || null;
+
+        // Reply with the current prefix if a new prefix wasn't provided
+        if (!prefix) {
+            return await message.reply({
+                content: `My prefix is \`${await guildManager.fetchPrefix(message.guild.id)}\``,
+                allowedMentions: { repliedUser: false }
+            });
+        }
+
+        // Set the prefix in the database
+        await guildManager.setPrefix(message.guild.id, prefix);
+
+        // Reply to the user with the new prefix
+        return await message.reply({
+            content: `Prefix has been set to \`${prefix}\``,
+            allowedMentions: { repliedUser: false }
+        });
+    }
+};
